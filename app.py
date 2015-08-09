1b56f4a3d9397db29998bf184db71a8f842b33b6
dzirtbry <dzirtbry@gmail.com>
2015-08-09 02:36:35 -0700

    Changing port

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
     conf = {
         'global': {
             'server.socket_host': '0.0.0.0',
-            'server.socket_port': 9999
+            'server.socket_port': int(os.environ.get('PORT', '5000'))
         },
         '/': {
             'tools.sessions.on': True,
