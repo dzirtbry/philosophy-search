034d0a7713fde8318569a953cafb18d8d26384bf
dzirtbry <dzirtbry@gmail.com>
2015-08-13 01:14:16 -0700

    Added result text

--- static/js/controllers/main.js
+++ static/js/controllers/main.js
@@ -18,47 +18,48 @@ angular.module('philosophySearchApp')
 
 
     $scope.trace = function () {
-      $scope.path = [];
+      //$scope.path = [];
       $scope.finished = false;
       var word = $scope.input.url.slice($scope.input.url.lastIndexOf('/') + 1);
       $scope.tracePath(word, $scope.input.url, $scope.path);
     };
 
     $scope.tracePath = function (name, url, path) {
-      this.hasDuplicates = function (array) {
-        var urlSet = new Set(array.map(function (t) {
-          return t.url
-        }));
-        return urlSet.size != array.length;
-      };
-
       var page = {name: name, url: url, type: ''};
       path.push(page);
+
       if (url.toLowerCase() === $scope.finishUrl.toLowerCase()) {
-        $scope.finised = true;
+        $scope.finished = true;
         page.type = 'philosophy';
-        return;
-      }
-      if (this.hasDuplicates(path)) {
-        $scope.finised = true;
-        for (var i = 0; i < path.length; i++) {
-          if (url.toLowerCase() === path[i].url.toLowerCase()) {
-            path[i].type = 'circle';
+      } else if (this.hasDuplicates(path)) {
+        $scope.finished = true;
+        path.map(function (element) {
+          if (url.toLowerCase() === element.url.toLowerCase()) {
+            element.type = 'circle';
           }
-        }
-
+        });
         page.type = 'circle';
-        return;
+      } else if (url.length == 0) {
+        page.type = 'last'
+        $scope.finished = true;
+      } else {
+        $http({
+          url: "/wiki",
+          method: "GET",
+          params: {"url": url}
+        }).success(function (data) {
+          console.log(data);
+          $scope.tracePath(data.name, data.url, path);
+        });
       }
-      $http({
-        url: "/wiki",
-        method: "GET",
-        params: {"url": url}
-      }).success(function (data) {
-        console.log(data);
-        $scope.tracePath(data.name, data.url, path);
-      });
 
+    };
+
+    $scope.hasDuplicates = function (array) {
+      var urlSet = new Set(array.map(function (t) {
+        return t.url
+      }));
+      return urlSet.size != array.length;
     };
 
   });
