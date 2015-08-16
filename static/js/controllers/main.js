ee663c64c1d9957fb048c0483663f6194f77ff12
dzirtbry <dzirtbry@gmail.com>
2015-08-16 16:47:46 -0700

    Refactored MainController

--- static/js/controllers/main.js
+++ static/js/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the philosopySearchApp
  */
 angular.module('philosophySearchApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function ($scope, $http, wiki) {
     $scope.input = {
       url: "https://en.wikipedia.org/wiki/Anarchy"
     };
@@ -16,32 +16,34 @@ angular.module('philosophySearchApp')
     $scope.finishUrl = "https://en.wikipedia.org/wiki/Philosophy";
     $scope.tracing = false;
 
-
-    $scope.trace = function () {
+    function reset() {
       $scope.path = [];
-      $scope.tracing = true;
-      var word = $scope.input.url.slice($scope.input.url.lastIndexOf('/') + 1);
+      $scope.tracing = false;
+    }
 
-      var lang = $scope.input.url.match("https?://([a-z]+)\.wikipedia.org")[1];
-      $http({
-        url: "/wiki/philosophy",
-        method: "GET",
-        params: {"lang": lang}
-      }).success(function (data) {
-        console.log(data);
-        $scope.finishUrl = data.url;
-        $scope.tracePath(word, $scope.input.url, $scope.path)
-      });
-    };
+    function extractLanguage(url) {
+      return url.match("https?://([a-z]+)\.wikipedia.org")[1];
+    }
 
-    $scope.tracePath = function (name, url, path) {
+    function extractWord(url) {
+      return url.slice($scope.input.url.lastIndexOf('/') + 1);
+    }
+
+    function hasDuplicates(array) {
+      var urlSet = new Set(array.map(function (t) {
+        return t.url
+      }));
+      return urlSet.size != array.length;
+    }
+
+    function tracePath(name, url, path) {
       var page = {name: decodeURIComponent(name), url: decodeURIComponent(url), type: ''};
       path.push(page);
 
       if (url.toLowerCase() === $scope.finishUrl.toLowerCase()) {
         $scope.tracing = false;
         page.type = 'philosophy';
-      } else if (this.hasDuplicates(path)) {
+      } else if (hasDuplicates(path)) {
         $scope.tracing = false;
         path.map(function (element) {
           if (page.url.toLowerCase() === element.url.toLowerCase()) {
@@ -53,23 +55,23 @@ angular.module('philosophySearchApp')
         page.type = 'last';
         $scope.tracing = false;
       } else {
-        $http({
-          url: "/wiki",
-          method: "GET",
-          params: {"url": url}
-        }).success(function (data) {
+        wiki.getNextPage(url).success(function (data) {
           console.log(data);
-          $scope.tracePath(data.name, data.url, path);
+          tracePath(data.name, data.url, path);
         });
       }
+    }
 
-    };
-
-    $scope.hasDuplicates = function (array) {
-      var urlSet = new Set(array.map(function (t) {
-        return t.url
-      }));
-      return urlSet.size != array.length;
+    $scope.trace = function () {
+      reset();
+      $scope.tracing = true;
+      var word = extractWord($scope.input.url);
+      var lang = extractLanguage($scope.input.url);
+      wiki.getPhilosophyPage(lang).success(function (data) {
+        console.log(data);
+        $scope.finishUrl = data.url;
+        tracePath(word, $scope.input.url, $scope.path)
+      });
     };
 
   });
--- static/js/service/wiki.js
+++ static/js/service/wiki.js
@@ -1,21 +1,19 @@
 angular.module('philosophySearchApp', [])
   .factory('wiki', function ($http) {
     var wikiService = {
-      //getFirstLink: getFirstLink,
-      //getPhilosophyLink: getPhilosophyLink;
-      getFirstLink: function (url) {
-        return $http({
-          url: "/wiki",
-          method: "GET",
-          params: {"url": url}
-        });
-      },
-
-      getPhilosophyLink: function (lang) {
+      getPhilosophyPage: function (lang) {
         return $http({
           url: "/wiki/philosophy",
           method: "GET",
           params: {"lang": lang}
+        });
+      },
+
+      getNextPage: function (url) {
+        return $http({
+          url: "/wiki",
+          method: "GET",
+          params: {"url": url}
         });
       }
     };
