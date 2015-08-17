e41e14971de381ed6812202202301a8501682636
dzirtbry <dzirtbry@gmail.com>
2015-08-16 17:50:15 -0700

    Binded enter key using directives

---
+++ static/js/directives/common.js
@@ -0,0 +1,13 @@
+angular.module('philosophySearchApp')
+  .directive('keyEnter', function () {
+    return function (scope, element, attrs) {
+      element.bind("keydown keypress", function (event) {
+        if (event.which === 13) {
+          scope.$apply(function () {
+            scope.$eval(attrs.keyEnter, {'event': event});
+          });
+          event.preventDefault();
+        }
+      });
+    };
+  });
