ba5c16a6e7fd164a168c6591e433e6a97862aeba
dzirtbry <dzirtbry@gmail.com>
2015-08-16 23:26:27 -0700

    Removed social controller

--- static/js/controllers/social.js
+++
@@ -1,7 +0,0 @@
-angular.module('philosophySearchApp')
-  .controller('SocialCtrl', ['$scope', function ($scope) {
-    $scope.logEvent = function(socialProvoder) {
-      ga('send', 'event', 'social', 'shared', socialProvoder);
-    }
-  }]
-);
