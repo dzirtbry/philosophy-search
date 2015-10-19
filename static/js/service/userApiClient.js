2ea48179acd00352adf62b848a7af4399892c3b7
dzirtbry <dzirtbry@gmail.com>
2015-10-18 20:56:31 -0700

    Separated api calls from logic around them.

---
+++ static/js/service/userApiClient.js
@@ -0,0 +1,66 @@
+/**
+ * Created by dzirtbry on 10/18/15.
+ */
+var TOKEN_URL = "http://192.168.0.2:8888/training-planner/api/oauth/token";
+var USERS_URL = "http://192.168.0.2:8888/training-planner/api/users";
+var ME_URL = "http://192.168.0.2:8888/training-planner/api/users/me";
+
+angular.module('philosophySearchApp')
+  .factory('userApiClient', ['$http', '$q', function ($http, $q) {
+    var userApiClient = {
+      /**
+       * Login by username and password.
+       * @param username username
+       * @param password password
+       * @returns {*}
+       */
+      login: function (username, password) {
+        return $http({
+          url: TOKEN_URL,
+          method: "POST",
+          withCredential: true,
+          headers: {
+            'Content-Type': "application/x-www-form-urlencoded",
+            'Authorization': "Basic MzUzYjMwMmM0NDU3NGY1NjUwNDU2ODdlNTM0ZTdkNmE6Mjg2OTI0Njk3ZTYxNWE2NzJhNjQ2YTQ5MzU0NTY0NmM="
+          },
+          data: $.param({
+            username: username,
+            password: password,
+            grant_type: "password"
+          })
+        });
+      },
+      register: function (username, password) {
+        return $http({
+          url: USERS_URL,
+          method: "POST",
+          headers: {'Content-Type': "application/json; charset=utf-8"},
+          data: {
+            user: {
+              email: username
+            },
+            password: password
+          }
+        });
+      },
+      logout: function () {
+        return $q(function (resolve, reject) {
+          resolve({});
+        });
+      },
+      me: function () {
+        return $http({
+          url: ME_URL,
+          method: "GET",
+          withCredential: true
+        });
+      },
+      user: function (username) {
+        return $q(function (resolve, reject) {
+          reject({data: "Not supported."});
+        });
+      }
+    };
+    return userApiClient;
+  }]
+);
--- static/js/service/userService.js
+++ static/js/service/userService.js
@@ -1,12 +1,10 @@
 /**
  * Created by dzirtbry on 10/17/15.
  */
-var TOKEN_URL = "http://192.168.0.2:8888/training-planner/api/oauth/token";
-var USERS_URL = "http://192.168.0.2:8888/training-planner/api/users";
-var ME_URL = "http://192.168.0.2:8888/training-planner/api/users/me";
+
 
 angular.module('philosophySearchApp')
-  .factory('userService', ['$http', '$q', 'tokenStore', function ($http, $q, tokenStore) {
+  .factory('userService', ['userApiClient', 'tokenStore', function (userApiClient, tokenStore) {
 
     var userService = {
       model: {
@@ -15,80 +13,39 @@ angular.module('philosophySearchApp')
       },
 
       login: function (username, password) {
-        return $q(function (resolve, reject) {
-          $http({
-            url: TOKEN_URL,
-            method: "POST",
-            withCredential: true,
-            headers: {
-              'Content-Type': "application/x-www-form-urlencoded",
-              'Authorization': "Basic MzUzYjMwMmM0NDU3NGY1NjUwNDU2ODdlNTM0ZTdkNmE6Mjg2OTI0Njk3ZTYxNWE2NzJhNjQ2YTQ5MzU0NTY0NmM="
-            },
-            data: $.param({
-              username: username,
-              password: password,
-              grant_type: "password"
-            })
-          }).then(function (response) {
-            console.log("Successful login, " + response.data.access_token);
-            console.log(response);
+        return userApiClient.login(username, password)
+          .then(function (response) {
             tokenStore.save(response.data);
             userService.model.isLoggedIn = true;
             userService.model.userName = username;
-            resolve(response);
-          }, function (response) {
-            console.log("Non-Successful login, " + response.data);
+          }, function (reason) {
             userService.model.isLoggedIn = false;
             userService.model.userName = '';
-            reject(response);
-          })
-        });
+          }
+        );
       },
 
       me: function () {
-        return $q(function (resolve, reject) {
-          if (!tokenStore.isValid()) {
-            reject({data: "Invalid token"});
-            return;
-          }
-          $http({
-            url: ME_URL,
-            method: "GET",
-            withCredential: true
-          }).then(function (response) {
-            userService.model.isLoggedIn = true;
-            userService.model.userName = response.data.email;
-            resolve(response);
-          }, function (response) {
-            userService.model.isLoggedIn = false;
-            userService.model.userName = '';
-            reject(response);
-          })
+        return userApiClient.me().then(function (response) {
+          userService.model.isLoggedIn = true;
+          userService.model.userName = response.data.email;
+        }, function (reason) {
+          userService.model.isLoggedIn = false;
+          userService.model.userName = '';
         });
       },
 
       logout: function () {
         // Cleanup cookies and invalidate token
-        return $q(function (resolve, reject) {
+        return userApiClient.logout().finally(function () {
           userService.model.isLoggedIn = false;
           userService.model.userName = '';
           tokenStore.remove();
-          resolve({});
         });
       },
 
       register: function (username, password) {
-        return $http({
-          url: USERS_URL,
-          method: "POST",
-          headers: {'Content-Type': "application/json; charset=utf-8"},
-          data: {
-            user: {
-              email: username
-            },
-            password: password
-          }
-        });
+        return userApiClient.register(username, password);
       },
 
       isLoggedIn: function () {
