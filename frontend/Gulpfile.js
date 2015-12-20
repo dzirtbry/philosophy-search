e0d5f2c5460057595293a1597212663c617c0365
dzirtbry <dzirtbry@gmail.com>
2015-12-19 20:00:37 -0800

    bootswatch-superhero theme is now compiled from less.

--- frontend/Gulpfile.js
+++ frontend/Gulpfile.js
@@ -1,9 +1,27 @@
+var del = require('del');
 var gulp = require('gulp');
+var less = require('gulp-less');
+var print = require('gulp-print');
+var pref = require('gulp-autoprefixer');
 
-gulp.task('test', function() {
-
+gulp.task('test', function () {
+  console.log('No tests this far.')
 });
 
-gulp.task('dist', function() {
+gulp.task('clean', ['clean-styles']);
+
+gulp.task('clean-styles', function (done) {
+  del('./src/css/superhero.css');
+  done();
+});
+
+gulp.task('styles', ['clean-styles'], function () {
+  return gulp.src(['./src/less/superhero.less'])
+    .pipe(less())
+    .pipe(pref({browsers: ['last 2 versions', '> 5%']}))
+    .pipe(gulp.dest("./src/css/"));
+});
+
+gulp.task('dist', ['styles'], function () {
   console.log('Hello world');
 });
