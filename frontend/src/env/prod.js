919caf75bc32a341cc0785db9ce9a792383e12a4
dzirtbry <dzirtbry@gmail.com>
2015-12-22 22:48:35 -0800

    Added stub for prod configuration

---
+++ frontend/src/env/prod.js
@@ -0,0 +1,13 @@
+/**
+ * Created on 12/22/15.
+ *
+ * @author dzirtbry
+ */
+(function () {
+  'use strict';
+  angular.module('philosophySearchApp')
+    .constant('API', {
+      ENDPOINT: 'http://localhost:5000'
+    });
+
+})();
